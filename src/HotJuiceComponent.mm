64d02312573549bc868bd2df87476ef6ce89bc28
andorxornot <andorxornot@gmail.com>
2020-10-27 20:54:47 +0300

    Fixed for osx

--- src/HotJuiceComponent.mm
+++ src/HotJuiceComponent.mm
@@ -1,9 +1,11 @@
 #include <AppKit/NSOpenGL.h>
 #include "HotJuiceComponent.h"
  
-#ifndef  WIN32
+#if defined(JUCE_APP_VERSION)
 void HotJuiceComponent::makeTransparent()
 {
+	setOpaque(false);
+
 	NSOpenGLContext* context = (NSOpenGLContext*)openGLContext.getRawContext();
 	GLint aValue = 0;
 	[context setValues : &aValue forParameter : NSOpenGLCPSurfaceOpacity];
